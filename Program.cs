68b2d4b2cf9ebd255366c83430ebdc6ff69853a4
Billy Mathison <wlmathison@gmail.com>
2019-06-06 14:52:36 -0500

    Completing part 2 requirements

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace student_exercises
 {
@@ -15,35 +16,51 @@ namespace student_exercises
             Exercise DynamicRouting = new Exercise("Dynamic Routing", "React", 6);
             Exercise StateProps = new Exercise("State and Props", "React", 7);
             Exercise URLRouting = new Exercise("URL Routing", "React", 8);
+            Exercise Inheritance = new Exercise("Inheritance", "C#", 9);
 
-            List<Exercise> Exercises = new List<Exercise>();
-            Exercises.Add(StudentExercises);
-            Exercises.Add(Lists);
-            Exercises.Add(Dictionaries);
-            Exercises.Add(Classes);
-            Exercises.Add(ConditionalRouting);
-            Exercises.Add(DynamicRouting);
-            Exercises.Add(StateProps);
-            Exercises.Add(URLRouting);
+            List<Exercise> exercises = new List<Exercise>();
+            exercises.Add(StudentExercises);
+            exercises.Add(Lists);
+            exercises.Add(Dictionaries);
+            exercises.Add(Classes);
+            exercises.Add(ConditionalRouting);
+            exercises.Add(DynamicRouting);
+            exercises.Add(StateProps);
+            exercises.Add(URLRouting);
+            exercises.Add(Inheritance);
 
             Cohort c31 = new Cohort("31", 1);
             Cohort c32 = new Cohort("32", 2);
             Cohort c33 = new Cohort("33", 3);
 
+            List<Cohort> cohorts = new List<Cohort>();
+            cohorts.Add(c31);
+            cohorts.Add(c32);
+            cohorts.Add(c33);
+
             Student Billy = new Student("Billy", "Mathison", "Billy Mathison", 1);
             Student Jameka = new Student("Jameka", "Echols", "Jameka", 2);
             Student Alex = new Student("Alex", "Thacker", "Alex Thacker", 3);
             Student Jonathan = new Student("Jonathan", "Schaffer", "Jonathan Schaffer", 4);
-            Billy.Cohort = c31;
-            Jameka.Cohort = c31;
-            Alex.Cohort = c31;
-            Jonathan.Cohort = c31;
+            Student Chris = new Student("Chris", "Morgan", "Chris Blue", 5);
 
-            List<Student> Students = new List<Student>();
-            Students.Add(Billy);
-            Students.Add(Jameka);
-            Students.Add(Alex);
-            Students.Add(Jonathan);
+            c31.Students.Add(Billy);
+            Billy.Cohort = c31;
+            c31.Students.Add(Jameka);
+            Jameka.Cohort = c31;
+            c31.Students.Add(Alex);
+            Alex.Cohort = c31;
+            c31.Students.Add(Jonathan);
+            Jonathan.Cohort = c31;
+            c31.Students.Add(Chris);
+            Chris.Cohort = c31;
+
+            List<Student> students = new List<Student>();
+            students.Add(Billy);
+            students.Add(Jameka);
+            students.Add(Alex);
+            students.Add(Jonathan);
+            students.Add(Chris);
 
             Instructor Andy = new Instructor("Andy", "Collins", "andyc", "jokes", 1);
             Instructor Leah = new Instructor("Leah", "Hoefling", "Leah", "dancing", 2);
@@ -57,6 +74,12 @@ namespace student_exercises
             Jisie.Cohort = c33;
             c31.Instructors.Add(Kristen);
             Kristen.Cohort = c33;
+
+            List<Instructor> instructors = new List<Instructor>();
+            instructors.Add(Andy);
+            instructors.Add(Leah);
+            instructors.Add(Jisie);
+            instructors.Add(Kristen);
 
             Andy.AssignExercise(Billy, StudentExercises);
             Andy.AssignExercise(Billy, Lists);
@@ -90,14 +113,64 @@ namespace student_exercises
             Kristen.AssignExercise(Alex, URLRouting);
             Kristen.AssignExercise(Jonathan, StateProps);
             Kristen.AssignExercise(Jonathan, URLRouting);
+            Andy.AssignExercise(Billy, Inheritance);
 
-            foreach(Student student in Students) {
+            foreach (Student student in students)
+            {
                 Console.WriteLine($"{student.FirstName} {student.LastName}");
-                foreach(Exercise exercise in student.Exercises) {
+                foreach (Exercise exercise in student.Exercises)
+                {
                     Console.WriteLine($" - {exercise.Name} in {exercise.Language}");
                 }
-                Console.WriteLine("------------------------------");
+                Console.WriteLine("-------------------------------------------");
             }
+
+            Console.WriteLine("React Exercises:");
+            foreach (Exercise exercise in exercises.Where(exercise => exercise.Language == "React"))
+            {
+                Console.WriteLine($" - {exercise.Name}");
+            }
+            Console.WriteLine("-------------------------------------------");
+
+            Console.WriteLine("Cohort 31 Students:");
+            foreach (Student student in students.Where(student => student.Cohort == c31))
+            {
+                Console.WriteLine($" - {student.FirstName} {student.LastName}");
+            }
+            Console.WriteLine("-------------------------------------------");
+
+            Console.WriteLine("Cohort 31 Instructors:");
+            foreach (Instructor instructor in instructors.Where(instructor => instructor.Cohort == c31))
+            {
+                Console.WriteLine($" - {instructor.FirstName} {instructor.LastName}");
+            }
+            Console.WriteLine("-------------------------------------------");
+
+            Console.WriteLine("Students in alphabetical order:");
+            foreach (Student student in students.OrderBy(student => student.LastName))
+            {
+                Console.WriteLine($" - {student.FirstName} {student.LastName}");
+            }
+            Console.WriteLine("-------------------------------------------");
+
+            Console.WriteLine("Students not working on any exercises currently:");
+            foreach (Student student in students.Where(student => student.Exercises.Count == 0))
+            {
+                Console.WriteLine($" - {student.FirstName} {student.LastName}");
+            }
+            Console.WriteLine("-------------------------------------------");
+
+            Console.WriteLine("Student working on the most exercises currently:");
+            Student StudentWithMostExercises = students.OrderByDescending(student => student.Exercises.Count).ToList()[0];
+            Console.WriteLine($" - {StudentWithMostExercises.FirstName} {StudentWithMostExercises.LastName} : {StudentWithMostExercises.Exercises.Count}");
+            Console.WriteLine("-------------------------------------------");
+
+            Console.WriteLine("Number of students in each cohort:");
+            foreach (Cohort cohort in cohorts)
+            {
+                Console.WriteLine($" - {cohort.Name}: {cohort.Students.Count}");
+            }
+            Console.WriteLine("-------------------------------------------");
         }
     }
 }
